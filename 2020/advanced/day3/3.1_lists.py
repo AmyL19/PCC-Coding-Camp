8389a41d186bfa11f279235dc7042e1649817d98
dyuan0226 <david.yuan@tapad.com>
2020-08-25 19:29:57 -0400

    removed unnecessary files

---
+++ 2020/advanced/day3/3.1_lists.py
@@ -0,0 +1,9 @@
+#================================  EMPTY LISTS  ===============================
+
+a = []
+b = list()
+
+print(f"a is equal to b: {a == b}")
+print(f"the length of a is {len(a)}")
+print(f"the type of a and b are: {type(b)}")
+
